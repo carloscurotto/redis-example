cc997a96b8e69b2cab6fc79ef69d741689e0a1b8
carloscurotto <carloscurotto@gmail.com>
2012-12-29 14:38:19 -0300

    - adding test placeholder

---
+++ src/test/java/ar/com/carloscurotto/redis/TestRedisExample.java
@@ -0,0 +1,18 @@
+package ar.com.carloscurotto.redis;
+
+import junit.framework.TestCase;
+
+/**
+ * This class is a placeholder to allow git to create source test folders.
+ * 
+ * @author carloscurotto
+ *
+ */
+public class TestRedisExample extends TestCase {
+	
+	/**
+	 * This test is a placeholder to allow the placeholder test class to run.
+	 */
+	public void testPlaceholder() {}
+
+}
